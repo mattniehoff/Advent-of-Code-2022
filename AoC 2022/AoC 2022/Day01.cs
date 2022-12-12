0c56cdae6e72571ee5003883d18d082c7fc32ff6
Matt Niehoff <nieho003@umn.edu>
2022-12-12 10:50:40 -0600

    Completes Day 01.

---
+++ AoC 2022/AoC 2022/Day01.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AoC_2022
+{
+    internal class Day01 : IRunnableDay
+    {
+
+        public void Run()
+        {
+            Console.WriteLine("Hi from Day 01");
+
+            // Read each line, if not empty add to current elf
+            List<int> calorieTotals = new List<int>();
+            if (File.Exists(@"Inputs\day01input.txt"))
+            {
+                int currentCalories = 0;
+                foreach (string line in System.IO.File.ReadLines(@"Inputs\day01input.txt"))
+                {
+                    if (string.IsNullOrWhiteSpace(line))
+                    {
+                        calorieTotals.Add(currentCalories);
+                        currentCalories = 0;
+                    }
+                    else
+                    {
+                        int lineValue;
+                        int.TryParse(line, out lineValue);
+                        currentCalories += lineValue;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Max calories is: {calorieTotals.Max()}");
+        }
+
+    }
+}
---
+++ AoC 2022/AoC 2022/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AoC_2022;
+
+Console.WriteLine("Hello, World!");
+
+var day = new Day01();
+day.Run();
